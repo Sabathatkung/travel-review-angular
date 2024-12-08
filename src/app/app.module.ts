f829cae9b6354b11a97512aea45dfccaf1df7c6f
Sabathatkung <theethatgosboonmee@gmail.com>
2024-12-08 16:43:24 +0700

    Revert "envaronment review"
    This reverts commit 82117dfdd194f4c2f8f95d4d53291f171f28ebdb.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReviewComponent } from './review/review.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ReviewComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
